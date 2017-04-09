ab4f00ddb601fbcaffaca46bef9851c3349b3a70
Luan Nico <luanpotter27@gmail.com>
2017-04-09 15:22:29 -0300

    Fix 740

--- my-labs/f740.js
+++ my-labs/f740.js
@@ -5,7 +5,7 @@ var b = new labs.EnvBuilder();
 b.constObj({
 	name: 'epsilon',
 	latexName: '\\epsilon',
-	formula: 'V/d',
+	formula: '-V/d',
 	unit: 'V/m'
 });
 
@@ -20,13 +20,14 @@ b.constObj({
 	name: 'd',
 	unit: 'm',
 	value: '0.0072',
-	error: '0.00001'
+	error: '0.00005'
 });
 
 b.constObj({
 	name: 'rho',
 	latexName: '\\rho',
-	unit: 'kg/m^3',
+	unit: 'g/m^3',
+	multiplier: 'k',
 	value: '886',
 	error: 0
 });
@@ -68,13 +69,14 @@ b.constObj({
 b.constObj({
 	name: 'dt',
 	unit: 's',
-	value: 4,
+	value: 2,
 	error: 0
 });
 
 b.constObj({
 	name: 'q',
 	unit: 'm',
+	multiplier: 'm',
 	value: 0.1,
 	error: 0
 });
@@ -82,10 +84,9 @@ b.constObj({
 b.constObj({
 	name: 'q_prime',
 	unit: 'm\'',
-	value: 1.1,
+	value: 0.9,
 	error: 0.05
 });
-
 
 b.variableObj({
 	name: 'df_prime',
@@ -103,14 +104,14 @@ b.variableObj({
 	name: 'df',
 	latexName: '\delta_f',
 	unit: 'm',
-	formula: 'df_prime*q/q_prime/1000'
+	formula: 'df_prime*q/q_prime'
 });
 
 b.variableObj({
 	name: 'dr',
 	latexName: '\delta_r',
 	unit: 'm',
-	formula: 'dr_prime*q/q_prime/1000'
+	formula: 'dr_prime*q/q_prime'
 });
 
 b.variableObj({
@@ -131,45 +132,34 @@ b.variableObj({
 	name: 'a',
 	description: 'Raio da gotícula',
 	unit: 'm',
-	formula: '((b^2)/(2*p) + 9*eta*vf/(2*g*p))^(1/2) - b/(2*p)'
+	formula: '((b/(2*p))^2 + 9*eta*vf/(2*g*p))^(1/2) - b/(2*p)'
 })
 
 b.variableObj({
 	name: 'Vol',
-	description: 'Volume da esfera',
+	description: 'Volume da gotícula',
 	unit: 'm^3',
 	formula: '4/3*pi*a^3'
 });
 
 b.variableObj({
 	name: 'm',
-	description: 'Massa da esfera',
-	unit: 'kg',
+	description: 'Massa da gotícula',
+	unit: 'g',
 	formula: 'rho * Vol'
 });
 
 b.variableObj({
 	name: 'charge',
 	latexName: 'q',
-	description: 'Carga da esfera',
+	description: 'Carga da gotícula',
 	unit: 'C',
-	formula: 'm*g*(vf+vr)/(epsilon*vf)'
+	formula: 'm*g*(vf-vr)/(epsilon*vf)'
 });
 
-b.variableObj({
-	name: 'p1',
-	unit: '?',
-	formula: '(vf+vr)/vf'
-});
-
-b.variableObj({
-	name: 'p2',
-	unit: '?',
-	formula: 'm*g/epsilon'
-});
 var e = b.build();
 
-e.add('df_prime', [ 3.0,  1.0,  2.0,  5.5], 0.05);
-e.add('dr_prime', [-1.0, -2.3, -4.5, -7.0], 0.05);
+e.add('df_prime', [ 1.55, 1.3, 1.2, 1, 1.1, .85, 1.5, .5, .6, 1.4, 1.9 ], 0.05);
+e.add('dr_prime', [ -1.3, -1.15, -1.6, -.4, -1.4, .7, -1.7, -1.8, 2.8, .55, -2.3 ], 0.05);
 
-console.log(e.fullLatexTable(['vr', 'vf', 'charge'], 'Minha tabela', 'charge'));
+console.log(e.fullLatexTable(['charge'], 'Minha tabela', 'charge'));
