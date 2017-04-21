38beef7c3bf886a1cd71098db87420efc6fcef85
Luan Nico <luanpotter27@gmail.com>
2017-04-21 15:52:34 -0300

    plottinggit st g

--- my-labs/f540-exp3-1-b.js
+++ my-labs/f540-exp3-1-b.js
@@ -1,33 +1,52 @@
-const labs = require ('../test/labs');
+const labs = require('../test/labs');
+const plotly = require('plotly');
+const fs = require('fs');
+const _ = require('lodash');
 
-const erro_r = rs => rs.map(r => [r, 0.01*r + 1]);
-const erro_v = vs => vs.map(v => [v, 0.02*v + .05*.01]);
+const request = require('request');
+const download = function(uri, filename, callback) {
+    request.head(uri, function(err, res, body) {
+        let r = request(uri).pipe(fs.createWriteStream(filename));
+        if (callback) {
+            r.on('close', callback);
+        }
+    });
+};
 
-let rs = erro_r([ 9.9, 19.8, 49.5, 98.3, 195.2, 391, 686 ]);
-let vdec = erro_v([ 0.42, 0.45, 0.47, 0.48, 0.49, 0.5, 0.51 ]);
-let vce = erro_v([ 2.71, 3.04, 3.23, 3.29, 3.32, 3.31, 3.33 ]);
+
+const build = cb => {
+    fs.readFile(require('os').homedir() + '/.plotly.data', 'utf8', function(err, data) {
+        let a = data.trim().split(':');
+        cb(plotly(a[0], a[1]));
+    });
+};
+
+const erro_r = rs => rs.map(r => [r, 0.01 * r + 1]);
+const erro_v = vs => vs.map(v => [v, 0.02 * v + .05 * .01]);
+
+let rs = erro_r([9.9, 19.8, 49.5, 98.3, 195.2, 391, 686]);
+let vdec = erro_v([0.42, 0.45, 0.47, 0.48, 0.49, 0.5, 0.51]);
+let vce = erro_v([2.71, 3.04, 3.23, 3.29, 3.32, 3.31, 3.33]);
 
 let create = () => {
-	let b = new labs.EnvBuilder();
+    let b = new labs.EnvBuilder();
 
-	b.constObj({ name: 'v_cc', latexName: 'V_{CC}', unit : 'V', value: 15, error: 0 });
-	b.constObj({ name: 'r4', latexName: 'R_4', unit : '\\Omega', value: 1, multiplier: 'k', error: 0 });
+    b.constObj({ name: 'v_cc', latexName: 'V_{CC}', unit: 'V', value: 15, error: 0 });
+    b.constObj({ name: 'r4', latexName: 'R_4', unit: '\\Omega', value: 1, multiplier: 'k', error: 0 });
 
-	b.variableObj({ name: 'r_dec', latexName: 'R_{dec}', unit : '\\Omega' });
-	b.variableObj({ name: 'v_dec', latexName: 'V_{dec}', unit : 'V' });
-	b.variableObj({ name: 'i_dec', latexName: 'I_{dec}', unit : 'A', formula: 'v_dec/r_dec' });
+    b.variableObj({ name: 'r_dec', latexName: 'R_{dec}', unit: '\\Omega' });
+    b.variableObj({ name: 'v_dec', latexName: 'V_{dec}', unit: 'V' });
+    b.variableObj({ name: 'i_dec', latexName: 'I_{dec}', unit: 'A', formula: 'v_dec/r_dec' });
 
-	b.variableObj({ name: 'v_ce', latexName: 'V_{CE}', unit : 'V' });
-	b.variableObj({ name: 'i_ce', latexName: 'I_{CE}', unit : 'A', formula : '(v_cc - v_ce)/r4' });
+    b.variableObj({ name: 'v_ce', latexName: 'V_{CE}', unit: 'V' });
+    b.variableObj({ name: 'i_ce', latexName: 'I_{CE}', unit: 'A', formula: '(v_cc - v_ce)/r4' });
 
-	b.variableObj({ name: 'beta', latexName: '\\beta', unit : '', formula : 'i_ce/i_dec' });
-
-	return b;
+    return b;
 };
 
 let b = create();
 let e = b.build();
-e.add('r_dec', rs);
+e.add('r_dec', rs, 'k');
 e.add('v_dec', vdec);
 e.add('v_ce', vce);
 
@@ -36,5 +55,27 @@ console.log(e.fullLatexTable(['i_ce', 'i_dec'], 'nome', 'label'));
 console.log('origin');
 console.log(e.originTable(['i_ce', 'i_dec']));
 
-console.log('waat?');
-console.log(e.fullLatexTable(['beta'], 'nome', 'label'));
+let table = _.unzip(e.originTable(['i_dec', 'i_ce']).split('\n').slice(1).map(a => a.split('\t')));
+var data = [{
+    x: table[0],
+    y: table[2],
+    mode: 'markers',
+    error_x: {
+        type: "data",
+        array: table[1],
+        visible: true
+    },
+    error_y: {
+        type: "data",
+        array: table[3],
+        visible: true
+    },
+    type: "scatter"
+}];
+var graphOptions = { filename: "basic-error-bar", fileopt: "overwrite" };
+
+build(plotly => {
+    plotly.plot(data, graphOptions, function(error, data) {
+        download(data.url + '.png', 'chart.png');
+    });
+});
