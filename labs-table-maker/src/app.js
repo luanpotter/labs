1fbeccd12643c99eeea75c925f4a39d93e4478f8
Luan Nico <luanpotter27@gmail.com>
2017-04-28 18:59:24 -0300

    progress on tablemaker

--- labs-table-maker/src/app.js
+++ labs-table-maker/src/app.js
@@ -21,9 +21,15 @@ jQuery(function ($) {
 });
 
 const parse = src => {
-  let data = _.unzip(src.split('\n').map(l => l.split('\t')));
-  let hasHeader = data.map(el => el[0]).some(el => /[a-zA-Z]/.test(el)));
-  !hasHeaders && data.length % 2 && throw 'Precisa de número par de colunas, ou cabeçalhos...';
-  let headers = hasHeaders ? data.shift() : _.range(data.length/2).map(i => 'v' + i);
+  let data = src.split('\n').map(l => l.split(/[ \t]+/));
+  let hasHeaders = data.map(el => el[0]).some(el => /[a-zA-Z]/.test(el));
+  if (!hasHeaders && data.length % 2 === 1) {
+    throw 'Precisa de número par de colunas, ou cabeçalhos...';
+  }
+  let headers = hasHeaders ? data.shift() : _.range(data.length/2).map(i => ['v' + i, 'e_v' + i]);
+  let u = _.uniq(data.map(el => el.length));
+  if (u.length !== 1 || u[0] !== headers.length) {
+    throw 'Todas as linhas precisam ter o mesmo número de colunas.';
+  }
   return 'asd' + src + '!';
 };
